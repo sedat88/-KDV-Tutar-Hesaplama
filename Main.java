6c0f453695084919c94f353b1e5df0fcb1c69bcf
SEDAT <121808551+sedat88@users.noreply.github.com>
2023-01-18 21:14:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double tutar,kdvOran = 0.8 , kdvTutar,kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz : " );
+        tutar = input.nextDouble();
+
+       kdvTutar = tutar * kdvOran;
+       kdvliTutar = tutar + kdvTutar;
+
+       System.out.println("KDV'siz tutar :" + tutar);
+       System.out.println("KDV Oranı :" + kdvOran);
+       System.out.println("KDV Tutarı :" + kdvTutar);
+       System.out.println("KDV'li Tutar : " + kdvliTutar);
+
+
+
+
+
+
+
+
+    }
+}
